42ee1a65f05b226310dc88763e869f61971730c0
sjungwon <sjungwon03@gmail.com>
2023-08-12 21:33:13 +0900

    test: memberController

--- src/member/member.controller.spec.ts
+++ src/member/member.controller.spec.ts
@@ -1,18 +1,27 @@
+import { UpdatePasswordDto } from './dto/update-password.dto';
 import { Test, TestingModule } from '@nestjs/testing';
 import { MemberController } from './member.controller';
 import { MemberService } from './member.service';
-import { getRepositoryToken } from '@nestjs/typeorm';
-import { Member } from './entities/member.entity';
+import { Role } from './enum/Role';
+import { MemberProfile } from './dto/member-profile.dto';
+import { JwtMemberDto } from 'src/auth/dto/jwt-member.dto';
 
 describe('MemberController', () => {
   let controller: MemberController;
+  const service: Partial<MemberService> = {
+    getProfile: jest.fn(),
+    updateNickname: jest.fn(),
+    updatePassword: jest.fn(),
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [MemberController],
       providers: [
-        MemberService,
-        { provide: getRepositoryToken(Member), useValue: {} },
+        {
+          provide: MemberService,
+          useValue: service,
+        },
       ],
     }).compile();
 
@@ -21,5 +30,57 @@ describe('MemberController', () => {
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  describe('getProfile', () => {
+    it('정상 프로필 반환', async () => {
+      const memberProfile = new MemberProfile();
+      memberProfile.nickname = 'nickname';
+      memberProfile.createdAt = new Date();
+      memberProfile.updatedAt = new Date();
+      jest.spyOn(service, 'getProfile').mockResolvedValue(memberProfile);
+      const jwtMemberDto = new JwtMemberDto(1, 'nickname', Role.USER);
+      const profile = await controller.getProfile(jwtMemberDto);
+      expect(profile).toEqual(memberProfile);
+    });
+  });
+
+  describe('updateNickname', () => {
+    it('이전 닉네임과 동일한 닉네임으로 요청 시 바로 성공', async () => {
+      const spy = jest.spyOn(service, 'updateNickname');
+      const jwtMemberDto = new JwtMemberDto(1, 'nickname', Role.USER);
+      const success = await controller.updateNickname(jwtMemberDto, 'nickname');
+      expect(success).toEqual('success');
+      expect(spy).not.toBeCalled();
+    });
+
+    it('업데이트 성공시 success 반환', async () => {
+      const spy = jest.spyOn(service, 'updateNickname');
+
+      const jwtMemberDto = new JwtMemberDto(1, 'nickname', Role.USER);
+      const success = await controller.updateNickname(
+        jwtMemberDto,
+        'newNickname',
+      );
+      expect(success).toEqual('success');
+      expect(spy).toBeCalled();
+    });
+  });
+
+  describe('updatePassword', () => {
+    it('비밀번호 업데이트 성공시 success 반환', async () => {
+      const spy = jest.spyOn(service, 'updatePassword');
+
+      const jwtMemberDto = new JwtMemberDto(1, 'nickname', Role.USER);
+      const updatePasswordDto = new UpdatePasswordDto();
+      updatePasswordDto.prevPassword = 'password';
+      updatePasswordDto.newPassword = 'newPassword';
+      const success = await controller.updatePassword(
+        jwtMemberDto,
+        updatePasswordDto,
+      );
+      expect(success).toEqual('success');
+      expect(spy).toBeCalled();
+    });
   });
 });
